5a26be62be7818512ed5003259d309d8ed817ad9
CasperTheFriendlyGhost135 <symmetricalguacamole@gmail.com>
2021-10-17 10:46:54 -0700

    MARK I

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ import math
 
 pygame.init()
 
-# Constants
+# ConstantFMos
 FOOD_PIC = pygame.image.load("assets/food.png")
 BLOB_PIC = pygame.image.load("assets/blob.png")
-MOVEMENT = 10.00
 DAYS = 0
+MUTATION_PERCENTAGE = 10.00
 
 screen = pygame.display.set_mode((900, 800))
 pygame.display.set_caption("The Evolution Game")
@@ -16,7 +16,7 @@ pygame.display.set_caption("The Evolution Game")
 
 class Blob:
     def __init__(
-            self, x, y, speed=4, num_days_without_food=1, sense=3, num_babies=1, lifespan=5
+            self, x, y, speed=10, num_days_without_food=1, sense=50, num_babies=1, lifespan=5
     ):
         self.num_babies = num_babies
         self.num_days_without_food = num_days_without_food
@@ -31,6 +31,31 @@ class Blob:
             f"Blob({self.x}, {self.y}, {self.speed}, {self.num_days_without_food}, {self.sense},"
             f"{self.num_babies}, {self.lifespan})"
         )
+
+    def makeBaby(self, max_mutation_percentage):
+        Baby_X_Cord = self.x
+        Baby_Y_Cord = self.y
+
+        # for now stay the same
+        baby_num_days_without_food = self.num_days_without_food
+        baby_num_babies = self.num_babies
+        baby_life_span = self.lifespan
+
+        # baby speed
+        baby_speed = 10
+        baby_speed_probability = random.randint(0, 2)
+        if baby_speed_probability > 1:
+            baby_speed = self.speed * (1 + max_mutation_percentage)
+        if baby_speed_probability < 1:
+            baby_speed = self.speed / (1 + max_mutation_percentage)
+        baby_sense_probability = random.randint(0, 2)
+        if baby_sense_probability > 1:
+            baby_sense = self.sense * (1 + max_mutation_percentage)
+        if baby_sense_probability < 1:
+            baby_sense = self.sense / (1 + max_mutation_percentage)
+
+        return Blob(Baby_X_Cord, Baby_Y_Cord, baby_speed, baby_num_days_without_food, baby_sense, baby_num_babies,
+                    baby_life_span)
 
     def blit(self):
         global screen
@@ -73,15 +98,15 @@ while running:
     for c in blobs:
         x_heads_tails = random.randint(0, 2)
         if x_heads_tails > 1:
-            c.x += MOVEMENT
+            c.x += c.speed
         elif x_heads_tails < 1:
-            c.x -= MOVEMENT
+            c.x -= c.speed
 
         y_heads_tails = random.randint(0, 2)
         if y_heads_tails > 1:
-            c.y += MOVEMENT
+            c.y += c.speed
         if y_heads_tails < 1:
-            c.y -= MOVEMENT
+            c.y -= c.speed
 
         if c.x > 850:
             c.x = 850
@@ -93,13 +118,14 @@ while running:
             c.y = 0
 
         for d in foods:
-            if distance(c.x, c.y, d.x, d.y) < 20:
+            if distance(c.x, c.y, d.x, d.y) < 150:
                 foods.remove(d)
         if len(foods) == 0:
             for e in blobs:
                 print(e.__repr__())
             foods: list[Food] = [Food(random.randint(50, 850), random.randint(50, 750)) for _ in range(9)]
-            DAYS += DAYS
+            DAYS += 1
+            print(DAYS)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
